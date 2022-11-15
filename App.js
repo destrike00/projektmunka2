0324ec233e2f0e28cf0ce9801c54e7c545eb4df9
destrike00 <k.aron147@gmail.com>
2022-11-15 20:15:03 +0100

    Google maps integracio

--- App.js
+++ App.js
@@ -2,15 +2,35 @@ import * as React from "react";
 import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { Flex, Stack, HStack } from "native-base";
+import { Flex, Stack, HStack, Box, Text } from "native-base";
 import { NativeBaseProvider } from "native-base";
+import { dustrinTheme } from "./src/style/theme";
+import Map from "./src/components/maps";
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
+    <View style={{ flex: 1, height: "100%" }}>
+      <Box w="95%" marginTop="10px" variant="whitebox" alignSelf="center">
+        <Text>a faszom kivan</Text>
+      </Box>
+      <HStack
+        w="95%"
+        h="100%"
+        alignSelf="center"
+        justifyContent="space-between"
+      >
+        <Box w="50%" h="83%" variant="whitebox" p="1">
+          <Map />
+        </Box>
+
+        <Flex h="100%" w="45%" flexDirection="column">
+          <Box w="100%" h="40%" variant="whitebox">
+            <Text>a faszom kivan</Text>
+          </Box>
+          <Box w="100%" h="40%" variant="whitebox">
+            <Text>a faszom kivan</Text>
+          </Box>
+        </Flex>
+      </HStack>
     </View>
   );
 }
@@ -27,7 +47,7 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={dustrinTheme}>
       <NavigationContainer>
         <Drawer.Navigator
           initialRouteName="Home"
