83e8c175a96f9640dec89590d1af50f0e66120ce
destrike00 <k.aron147@gmail.com>
2022-11-15 20:16:25 +0100

    Revert "Google maps integracio"
    This reverts commit 0324ec233e2f0e28cf0ce9801c54e7c545eb4df9.

--- src/style/colors.js
+++
@@ -1,5 +0,0 @@
-export const PrimaryColor = '#012340';
-export const SecondaryColor = '#496D8C';
-export const Background='#FDFDFD';
-export const PrimaryButton='#04BF68';
-export const SecondaryButton='#025949';
